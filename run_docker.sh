cbcdf3337989722a8eaa57b3b24a43cb58c10768
Thomas Welsch <ttww@gmx.de>
2024-11-27 17:27:29 +0100

    Update docker run

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,3 @@
-docker run --rm -it --env-file .env klassenarbeiten
+#docker run --rm -it --env-file .env klassenarbeiten
+
+docker run -d --restart unless-stopped --name klassenarbeiten --env-file .env klassenarbeiten
